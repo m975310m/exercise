33d3a604029da85fa68af8bc0eed169a16ea7592
m975310m <a88331144a@gmail.com>
2020-08-21 20:57:14 +0800

    2020/08/21-補齊全部錯誤解釋

--- src/DesError.java
+++ src/DesError.java
@@ -15,8 +15,8 @@ public class DesError
 	//NumberFormatException --> 當應用程序試圖將字元串轉換成一種數值型別，但該字元串不能轉換為適當格式時，拋出該異常
 	//IOException --> 當發生某種 I/O 異常時，拋出此異常。此類別是失敗或中斷的 I/O 操作產生的異常的通用類別
 	//ClassCastException --> 當試圖將物件強制轉換為不是實例的子類別時，拋出該異常
-	//ClassFormatException --> 
-	//StringIndexOutOfBoundsException --> 此異常由 String 方法拋出，指示索引或者為負，或者超出字元串的大小。對諸如 charAt 的一些方法，當索引等於字元串的大小時，也會拋出該異常
+	//ClassFormatException --> class檔案損壞了,或者jar包壞了
+	//StringIndexOutOfBoundsException --> 此異常由 String方法拋出，指示索引或者為負，或者超出字元串的大小。對諸如 charAt 的一些方法，當索引等於字元串的大小時，也會拋出該異常
 	
 	public static void main(String[] args)
 	{
@@ -69,9 +69,6 @@ public class DesError
 		//ClassCastException
 		//	    Object x = new Integer(0);
 		//	    System.out.println((String)x);
-		
-		//ClassFormatException
-		
 		
 		//StringIndexOutOfBoundsException
 		//		String str = "ABCD";
